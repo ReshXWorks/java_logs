0156356074862e141e3e516528d72e62b8111b97
Reshma <iamreshmailango@gmail.com>
2025-07-24 01:14:46 +0530

    Create bubblesort implementation

---
+++ practice/bubblesort.java
@@ -0,0 +1,25 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {1,2,3,45,67}; //use separate for loop to get user input for unsorted array if needed
+	    for (int i=0; i<arr.length-1; i++){
+	        boolean flag = true;
+	        for (int j=0; j<arr.length-1; j++){
+	            if (arr[j]>arr[j+1]){
+	                flag =false;
+	                int temp = arr[j];
+	                arr[j]= arr[j+1];
+	                arr[j+1]=temp;
+	            }
+	        }
+	        if (flag){
+	            //System.out.println("firstloop!");
+	            break;
+	        }
+	        //System.out.println("will it print?");
+	    }
+	    for(int a: arr){
+	        System.out.println(a);
+	    }
+	}
+}
