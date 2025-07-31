667a5c7212122051e33fe930304747a18c99ff6b
NickWilde <iamreshmailango@gmail.com>
2025-07-31 16:17:29 +0530

    Update Quicksort1.java with do-while loops-classic hoare

--- practice/Quicksort1.java
+++ practice/Quicksort1.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
-public class Quicksort1{
-    public static void main(String[] args){
-        int[] array = {2,6,1,8,3,4};
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {4,2,7,3,8,1};
         quicksort(array, 0, array.length-1);
-        for (int a: array){
+        for(int a: array){
             System.out.print(a + " ");
         }
     }
     public static void quicksort(int[] arr, int low, int high){
-        if (low >= high){
+        if (low>=high){
             return;
         }
+        //hoare's algorithm: two pointers: i,j -take w.r.t passed parameters: low, high
+        int i = low-1;
+        int j = high+1;
         int pivot = arr[low];
-         //hoare principle, two pointers: i,j -take w.r.t passed parameters: low, high
-        int i = low;
-        int j = high;
-        while (true){   //pointer-movement
-            while (arr[i]<pivot){ 
+        while(true){    ////pointer-movement
+            do{
                 i++;
-            }
-            while (arr[j]>pivot){
+            }while(i <= high && arr[i]<pivot);
+            do{
                 j--;
-            }
-            if (i>=j){
+            }while(j >= low && arr[j]>pivot);
+            
+            if(i>=j){
                 break;
             }
-            int temp = arr[i];  //swap logic
-            arr[i]=arr[j];
-            arr[j]=temp;
+            //swap logic
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-        quicksort(arr, low, j); 
+        quicksort(arr, low, j);
         quicksort(arr, j+1, high);
     }
 }
